7a9d644a931bbf18db467388af51c11aadc69cef
Greg May <Gregory.T.May-1@ou.edu>
2021-09-08 12:44:50 -0500

    office hours

---
+++ Office Hours/Sumof3 office hours/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sumof3_office_hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            double var1;
+            var1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number");
+            double var2;
+            var2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number");
+            double var3;
+            var3 = Convert.ToDouble(Console.ReadLine());
+
+            double sum;
+            sum = var1 + var2 + var3;
+
+            Console.WriteLine(sum.ToString("N3"));
+
+            const double luckynumber = 7.777;
+            Console.WriteLine(luckynumber * sum);
+
+
+            
+
+
+
+
+
+        }
+    }
+}
